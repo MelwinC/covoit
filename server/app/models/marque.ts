2ea4fe0c8fb12484c34536bd0b9f5de83d7f850e
Karbbone <clement.maillet56@gmail.com>
2024-11-24 00:33:25 +0100

    Models and create table

---
+++ server/app/models/marque.ts
@@ -0,0 +1,21 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, hasMany } from '@adonisjs/lucid/orm'
+import type { HasMany } from '@adonisjs/lucid/types/relations'
+import Voiture from '#models/voiture'
+
+export default class Marque extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @column()
+  declare nom: string
+
+  @hasMany(() => Voiture)
+  declare voiture: HasMany<typeof Voiture>
+}
