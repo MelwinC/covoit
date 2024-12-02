216a5c94919aec4d483ba16103ba5e34f8e05464
Karbbone <clement.maillet56@gmail.com>
2024-12-02 09:21:49 +0100

    [BACK] Show et store

--- server/app/controllers/villes_controller.ts
+++ server/app/controllers/villes_controller.ts
@@ -1,7 +1,7 @@
 import type { HttpContext } from '@adonisjs/core/http'
 import { VilleService } from '#services/ville_service'
 import { inject } from '@adonisjs/core'
-
+import { createVilleValidator } from '#validators/ville'
 @inject()
 export default class VillesController {
   constructor(private villeService: VilleService) {}
@@ -20,10 +20,26 @@ export default class VillesController {
   /**
    * Handle form submission for the create action
    */
-  async store({ request }: HttpContext) {}
+  async store({ request, response }: HttpContext) {
+    try {
+      const payload = await request.validateUsing(createVilleValidator)
+      const ville = await this.villeService.store(payload)
+      response.status(201).send(ville)
+    } catch (err) {
+      response.status(400).send({ error: err })
+    }
+  }
 
   /**
    * Show individual record
    */
-  async show({ params }: HttpContext) {}
+  async show({ params, response }: HttpContext) {
+    try {
+      const ville = await this.villeService.show(params.id)
+      response.status(200).send(ville)
+      return ville
+    } catch (error) {
+      response.status(404).send({ error: 'Impossible de récupérer la ville' })
+    }
+  }
 }
--- server/app/services/ville_service.ts
+++ server/app/services/ville_service.ts
@@ -1,5 +1,9 @@
 import Ville from '#models/ville'
 
+interface VillePayload {
+  ville: string
+  codePostal: number
+}
 export class VilleService {
   async index() {
     try {
@@ -7,6 +11,25 @@ export class VilleService {
       return villes
     } catch (error) {
       throw new Error('Erreur lors de la récupération des villes.')
+    }
+  }
+  async store(payload: VillePayload) {
+    try {
+      const ville = new Ville()
+      ville.ville = payload.ville
+      ville.codePostal = payload.codePostal
+      await ville.save()
+      return ville
+    } catch (error) {
+      throw new Error('Erreur lors de la création de la ville.')
+    }
+  }
+  async show(id: number) {
+    try {
+      const ville = await Ville.findOrFail(id)
+      return ville
+    } catch (error) {
+      throw new Error('Erreur lors de la récupération de la ville.')
     }
   }
 }
