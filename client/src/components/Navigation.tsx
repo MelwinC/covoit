d4cf49084f9d5b6ef0bb2ebb7528bc9961e22088
Theomrh <92977368+Theomrh@users.noreply.github.com>
2024-12-02 15:49:04 +0100

    feat(navbar): add navbar

---
+++ client/src/components/Navigation.tsx
@@ -0,0 +1,44 @@
+const Navigation = () => {
+  const pathname = window.location.pathname;
+  const isAuthPage = pathname === "/auth" || pathname === "/login";
+
+  if (isAuthPage) {
+    return null;
+  }
+
+  return (
+    <div className="w-full">
+      <div className="max-w-4xl mx-auto flex items-center justify-between py-4">
+        <div className="flex items-center gap-12">
+          <a href="/" className="font-semibold text-lg">
+            AlloCovoit
+          </a>
+          <ul className="flex items-center gap-6">
+            <li>
+              <a href="/">Trajet</a>
+            </li>
+            <li>
+              <a href="/">Mes trajets</a>
+            </li>
+            <li>
+              <a href="/">Messages</a>
+            </li>
+            <li>
+              <a href="/">Contact</a>
+            </li>
+          </ul>
+        </div>
+        <div>
+          <a
+            href="/auth"
+            className="text-white px-4 py-2.5 bg-black text-sm rounded-md"
+          >
+            Connexion
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navigation;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,9 +3,11 @@ import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Auth from "./components/Auth.tsx";
+import Navigation from "./components/Navigation.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
+    <Navigation />
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/auth" element={<Auth />} />
