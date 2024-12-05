69df9bed39b4861684bff752ef411c3f67e3f2dc
MelwinC <contact@melwinchenu.fr>
2024-12-05 13:33:29 +0100

    feat(sort): Sort villes by name

--- client/src/services/ville.ts
+++ client/src/services/ville.ts
@@ -4,7 +4,8 @@ import { Ville } from "@/types/ville";
 const api = KySingleton.getInstance();
 
 export async function getVilles(): Promise<Ville[]> {
-  return await api.get("villes").json();
+  const response: Ville[] = await api.get("villes").json();
+  return response.sort((ville1, ville2) => ville1.ville.localeCompare(ville2.ville));
 }
 
 export async function getVilleById(id: number): Promise<Ville> {
